370836d5b135022308300f4b914f4cf71039152f
Mickey <secondMickey1108@gmail.com>
2026-02-17 19:01:48 +0800

    fixed library stuff

--- Testing/Testing.cpp
+++ Testing/Testing.cpp
@@ -5,6 +5,17 @@ int main(int argc, char* argv[]) {
     Quasi::Debug::QInfo$("Execution Directory: {}", argv[0]);
     Quasi::Debug::QInfo$("Project Directory: {}", Test::PROJECT_DIRECTORY);
 
+    Quasi::HashMap<Quasi::String, int> map;
+    map.Insert("high", 2);
+    map.Insert("main", 0);
+    map.Insert("shadow", 4);
+    map.Insert("outline", 1);
+    map.Insert("glow", 3);
+    for (const auto& [key, val] : map) {
+
+        Quasi::Debug::QInfo$("{}: {} but got {}", key, val, map.Get(key));
+    }
+
     Test::TestManager tManager;
     tManager.OnInit();
     while (tManager.WindowIsOpen())
